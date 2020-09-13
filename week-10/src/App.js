96b1a36f51e837eae889e7904553918a58ffcb03
Sebin  Benjamin <sebinbenjamin@gmail.com>
2020-09-14 11:44:26 +1200

    feat: testing demo

--- week-10/src/App.js
+++ week-10/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from "./components/button/button";
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <Button></Button>
+      </div>
     </div>
   );
 }
